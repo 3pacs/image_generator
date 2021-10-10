988b2ba97e51d5174880b4502fcc338443c262c0
zainsarfraz <44726141+zainsarfraz@users.noreply.github.com>
2021-10-11 01:00:07 +0500

    Added gallary

--- app.py
+++ app.py
@@ -1,22 +1,90 @@
 """Simple flask app that generates images and displays on the browser"""
-from imager import makeImage
-from flask import Flask, render_template
+from PIL.Image import ImageTransformHandler
+from flask.helpers import url_for
+from imager import X, makeImage
+from flask import Flask, render_template,redirect
 import os
+import sqlite3
+from flask import g
 
 # Flask App configurations
-app = Flask(__name__)
-image_folder = os.path.join('static', 'images')
+app = Flask(__name__ )
+image_folder = os.path.join('static', 'output')
 app.config['UPLOAD_FOLDER'] = image_folder
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+DATABASE = 'db.sqlite'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route('/')
-@app.route('/home')
 def home():
     """Home Page view that has the image and a button that generates a new image"""
-    output = makeImage(20)
-    image = os.path.join(app.config['UPLOAD_FOLDER'], output)
-    return render_template("index.html", img_produce=image)
+    img = makeImage(1)
+    db = get_db()
+    image = os.path.join(app.config['UPLOAD_FOLDER'], img)
+    bg = os.path.join(app.config['UPLOAD_FOLDER'], "new/bg/"+img)
+    return render_template("index.html", img_produce=image,img_produce2=bg)
 
+@app.route('/gallary')
+def gallary():
+    images = []
+    for save in query_db('select * from saved'):
+        print(save[1], 'has the path ', save[0])
+        images.append({
+            'id':save[1],
+            'image':save[0]
+        })
+    return render_template("gallary.html",images=images)
+
+
+
+from shutil import copyfile
+import time
+@app.route('/add_to_gallary/<id>')
+def add_to_gallary(id):
+    
+    if id == 1:
+        name = int(time.time())
+        x = "static/saved/"+str(name)+".png"
+        copyfile("static/output/new/bg/img0.png", x)
+        cur = get_db().cursor()
+        cur.execute("""INSERT INTO saved(image) 
+               VALUES (?);""", (x,))
+        
+    else:
+        name = int(time.time())
+        x = "static/saved/"+str(name)+".png"
+        copyfile("static/output/img0.png", x)
+        cur = get_db().cursor()
+        cur.execute("""INSERT INTO saved(image) 
+               VALUES (?);""", (x,))
+    
+    get_db().commit()
+    return redirect(url_for("home"))
 
 if __name__ == '__main__':  # defining main
     app.run()
--- imager.py
+++ imager.py
@@ -1,109 +1,132 @@
-"""Image generation script"""
-import random
-import math
+import random, math, colorsys
+from PIL import Image, ImageDraw
 import os
-from PIL import Image
+from skimage.io import imread_collection
+
+import numpy as np
 
 random.seed()
 
-
 class X:
-    def eval(self, x, y):
-        return x
-
-    def __str__(self):
-        return "x"
-
+   def eval(self, x, y):
+      return x
+   
+   def __str__(self):
+      return "x"
 
 class Y:
-    def eval(self, x, y):
-        return y
-
-    def __str__(self):
-        return "y"
-
+   def eval(self, x, y):
+      return y
+   
+   def __str__(self):
+      return "y"
 
 class SinPi:
-    def __init__(self, prob):
-        self.arg = build(prob * prob)
+   def __init__(self, prob):
+      self.arg = build(prob * prob)
+   
+   def __str__(self):
+      return "sin(pi*" + str(self.arg) + ")"
 
-    def __str__(self):
-        return "sin(pi*" + str(self.arg) + ")"
-
-    def eval(self, x, y):
-        return math.sin(math.pi * self.arg.eval(x, y))
-
+   def eval(self, x, y):
+      return math.sin(math.pi * self.arg.eval(x,y))
 
 class CosPi:
-    def __init__(self, prob):
-        self.arg = build(prob * prob)
+   def __init__(self, prob):
+      self.arg = build(prob * prob)
 
-    def __str__(self):
-        return "cos(pi*" + str(self.arg) + ")"
+   def __str__(self):
+      return "cos(pi*" + str(self.arg) + ")"
 
-    def eval(self, x, y):
-        return math.cos(math.pi * self.arg.eval(x, y))
-
+   def eval(self, x, y):
+      return math.cos(math.pi * self.arg.eval(x,y))
 
 class Times:
-    def __init__(self, prob):
-        self.lhs = build(prob * prob)
-        self.rhs = build(prob * prob)
+   def __init__(self, prob):
+      self.lhs = build(prob * prob)
+      self.rhs = build(prob * prob)
 
-    def __str__(self):
-        return str(self.lhs) + "*" + str(self.rhs)
+   def __str__(self):
+      return str(self.lhs) + "*" + str(self.rhs)
 
-    def eval(self, x, y):
-        return self.lhs.eval(x, y) * self.rhs.eval(x, y)
+   def eval(self, x, y):
+      return self.lhs.eval(x,y) * self.rhs.eval(x,y)
 
+def build(prob = 0.69):
+   if random.random() < prob:
+      return random.choice([SinPi, CosPi, Times])(prob)
+   else:
+      return random.choice([X, Y])()
 
-def build(prob=0.99):
-    if random.random() < prob:
-        return random.choice([SinPi, CosPi, Times])(prob)
-    else:
-        return random.choice([X, Y])()
-
-
-def plotIntensity(exp, pixelsPerUnit=150):
+def plotIntensity(exp, pixelsPerUnit = 112):
     canvasWidth = 2 * pixelsPerUnit + 1
     canvas = Image.new("L", (canvasWidth, canvasWidth))
 
     for py in range(canvasWidth):
         for px in range(canvasWidth):
             # Convert pixel location to [-1,1] coordinates
-            x = float(px - pixelsPerUnit) / pixelsPerUnit
+            x = float(px - pixelsPerUnit) / pixelsPerUnit 
             y = -float(py - pixelsPerUnit) / pixelsPerUnit
-            z = exp.eval(x, y)
+            z = exp.eval(x,y)
 
             # Scale [-1,1] result to [0,255].
-            intensity = int(z * 127.5 + 127.5)
-            canvas.putpixel((px, py), intensity)
+            intensity = int(z * 169 + 69)
+            canvas.putpixel((px,py), intensity)
 
     return canvas
 
-
-def plotColor(redExp, greenExp, blueExp, pixelsPerUnit=150):
-    redPlane = plotIntensity(redExp, pixelsPerUnit)
+def plotColor(redExp, greenExp, blueExp, pixelsPerUnit = 112):
+    redPlane   = plotIntensity(redExp, pixelsPerUnit)
     greenPlane = plotIntensity(greenExp, pixelsPerUnit)
-    bluePlane = plotIntensity(blueExp, pixelsPerUnit)
+    bluePlane  = plotIntensity(blueExp, pixelsPerUnit)
     return Image.merge("RGB", (redPlane, greenPlane, bluePlane))
 
+def makeImage(numPics = 20):
+   with open("eqns.txt", 'w') as eqnsFile:
+      for i in range(numPics):
+         redExp = build()
+         greenExp = build()
+         blueExp = build()
 
-def makeImage(numPics=20):
-    with open("eqns.txt", 'w') as eqnsFile:
-        for i in range(numPics):
-            redExp = build()
-            greenExp = build()
-            blueExp = build()
+         eqnsFile.write("img" + str(i) + ":\n")
+         eqnsFile.write("red = " + str(redExp) + "\n")
+         eqnsFile.write("green = " + str(greenExp) + "\n")
+         eqnsFile.write("blue = " + str(blueExp) + "\n\n")
 
-            eqnsFile.write("img" + str(i) + ":\n")
-            eqnsFile.write("red = " + str(redExp) + "\n")
-            eqnsFile.write("green = " + str(greenExp) + "\n")
-            eqnsFile.write("blue = " + str(blueExp) + "\n\n")
+         image = plotColor(redExp, greenExp, blueExp)
 
-            image = plotColor(redExp, greenExp, blueExp)
-            picfolder = os.path.join('static', 'images')
-            img = "img" + str(i) + ".jpg"
-            pic1 = os.path.join(picfolder, img)
-            image.save(pic1)
-            return "img" + str(i) + ".jpg"
+         background = Image.open("alpha/bg.jpg")
+         mask = Image.open("alpha/har.jpg")
+         mask = mask.convert("L")
+         image.putalpha(mask)
+         overlay = Image.open("alpha/mask225.png")
+         #overlay = overlay.convert("RGBA")
+         image.save("static/output/img" + str(i) + ".png")
+         new_img = Image.blend(image, overlay, .5)
+         new_img.save("static/output/new/img" + str(i) + ".png")
+
+         background.paste(image, (0, 0), overlay)
+         background.save("static/output/new/bg/img" + str(i) + ".png")
+
+   return "img0.png"
+
+#makeImage(10)
+#reading folder for pngs
+
+#image_folder = 'output'
+
+#images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+#image_files = [image_folder+'/'+img for img in os.listdir(image_folder) if img.endswith(".png")]
+#img = 
+#for image in images:
+
+   #background = Image.open(img)
+   #overlay = Image.open("alpha/mask225.png")
+
+   #background = background.convert("RGBA")
+   #overlay = overlay.convert("RGBA")
+
+   #new_img = Image.blend(background, overlay, 0.5)
+   #new_img.save("output/new/img" + str(i) + ".png")
+
+
